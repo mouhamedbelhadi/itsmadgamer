06edbc51b7343529e16f4e9f90c9fed93538825a
mouhamedbelhadi <47292727+mouhamedbelhadi@users.noreply.github.com>
2019-02-03 10:25:56 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("541542823149961216")
+setInterval(function() {
+channel.send(`hal win win win hal win win winhal win win wi el malayin wunnnnnn hhh i want a credut fycjjjdifdsoingfsoignbosngisngisngoisngoisngsingsingjsiongsgnsoignsoinignjspinspi`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
